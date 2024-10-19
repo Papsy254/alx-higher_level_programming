05db2954c99c533867427bcde8f1d833e0c2efed
Papsy254 <126698870+Papsy254@users.noreply.github.com>
2024-10-19 10:42:42 +0300

    Add files via upload

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""Define a class Square."""
+"""Defines a class Square"""
 
 
 class Square:
-    """Represent a square."""
+    """Class that defines properties of square.
+    """
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-"""Define a class Square."""
+"""Defines a class Square"""
 
 
 class Square:
-    """Represent a square."""
+    """
+    Class that defines properties of square by: (based on 0-square.py).
 
+    Attributes:
+        size: size of a square (1 side).
+    """
     def __init__(self, size):
-        """Initialize a new Square.
+        """Creates new instances of square (1 side).
 
         Args:
-            size (int): The size of the new square.
+            size: size of the square.
         """
         self.__size = size
